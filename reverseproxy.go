4265ecb5a1f290ae4c0f208f45c32a9a10811249
Alexander Surma <surma@surmair.de>
2013-03-18 15:34:24 +0100

    Move reverse proxy code from frischluft to here

---
+++ reverseproxy.go
@@ -0,0 +1,20 @@
+package kartoffelsack
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+// NewSingleHostReverseProxy is an augmentation of
+// `net/http/httputil.NewSingleHostReverseProxy` which additionally sets
+// the `Host` header.
+func NewSingleHostReverseProxy(url *url.URL) *httputil.ReverseProxy {
+	rp := httputil.NewSingleHostReverseProxy(url)
+	oldDirector := rp.Director
+	rp.Director = func(r *http.Request) {
+		oldDirector(r)
+		r.Host = url.Host
+	}
+	return rp
+}
