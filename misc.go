7a09fe6a50b05c3d1f64eda471f1d7c7c3052e8c
Alexander Surma <surma@surmair.de>
2013-03-27 10:46:49 +0100

    Add DefaultEnv

--- misc.go
+++ misc.go
@@ -2,6 +2,7 @@ package k
 
 import (
 	"net/url"
+	"os"
 )
 
 // MustURL calls net/url.Parse() and panics if it returns a non-nil
@@ -12,4 +13,14 @@ func MustURL(rawurl string) *url.URL {
 		panic(err)
 	}
 	return u
+}
+
+// DefaultEnv returns the value of an environment variable, provided
+// it has been set. If it is unset (i.e. empty), the specified default
+// value is returned.
+func DefaultEnv(key, def string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return def
 }
