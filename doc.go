92a2825f134021c11abc298530ae3ea8e33c79a0
Alexander Surma <surma@surmair.de>
2013-03-27 10:05:22 +0100

    Rename package to k

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
-// Package katalysator is a bag of small, uncategorized code  supposed
+// Package k(atalysator) is a bag of small, uncategorized code supposed
 // to ease the development of applications in go.
-package katalysator
+package k
 
 const (
 	VERSION = "0.0.6"
