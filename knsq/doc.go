f0bc0fc06e1f9b22f853780241ce0711ab64cbf2
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-16 11:41:50 +0800

    Delete knsq directory

--- knsq/doc.go
+++
@@ -1,9 +0,0 @@
-// package knsq (“knusk”) is a collection of helper and convenience
-// functions for bitly’s nsq library.
-//
-// See: github.com/bitly/nsq
-package knsq
-
-const (
-	VERSION = "0.7.2"
-)
